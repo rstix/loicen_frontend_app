c8bd8614286b05ba369e3582a3fb47452de1198d
Roman Stix <roman.stix@digitalsunray.com>
2024-07-18 23:32:25 +0200

    some changes

--- src/components/files-sidebar/files.tsx
+++ src/components/files-sidebar/files.tsx
@@ -1,3 +1,113 @@
+// 'use client';
+// import { Sources } from '@/interfaces/messages';
+// import { useState, useEffect } from 'react';
+// import DraggableFile from './draggable-file';
+// import SortableList from './sortable-list';
+
+// interface FilesProps {
+//   sources: Sources[];
+//   id: string;
+// }
+
+// const Files = ({ sources, id }: FilesProps) => {
+//   const [updatedSources, setUpdatedSources] = useState<Sources[]>([]);
+//   const [visibleCount, setVisibleCount] = useState(1);
+//   const [visibleSources, setVisibleSources] = useState<Sources[]>([]);
+//   const [sortedOrder, setSortedOrder] = useState<string[]>([]);
+
+//   useEffect(() => {
+//     if (sources && sources.length > 0) {
+//       const relevantSources = sortByScore(
+//         uniqueSources(sources.filter((source) => source.id === id))
+//       );
+//       setUpdatedSources(relevantSources);
+//       setSortedOrder(relevantSources.map((source) => source.file_id));
+//     }
+//   }, [sources, id]);
+
+//   useEffect(() => {
+//     setVisibleSources((prevVisibleSources) => [
+//       ...prevVisibleSources,
+//       ...updatedSources.slice(prevVisibleSources.length, visibleCount),
+//     ]);
+//     setSortedOrder((prevVisibleSources) => [
+//       ...prevVisibleSources,
+//       ...updatedSources
+//         .slice(prevVisibleSources.length, visibleCount)
+//         .map((source) => source.file_id),
+//     ]);
+//   }, [updatedSources, visibleCount]);
+
+//   const uniqueSources = (sources: Sources[]): Sources[] => {
+//     const seen = new Set();
+//     return sources.filter((source) => {
+//       const duplicate = seen.has(source.title);
+//       seen.add(source.title);
+//       return !duplicate;
+//     });
+//   };
+
+//   const handleSourceClick = (source: Sources) => {
+//     setVisibleSources((prevSources) =>
+//       prevSources.map((s) =>
+//         s.title === source.title ? { ...s, relevant: !s.relevant } : s
+//       )
+//     );
+//   };
+
+//   const handleShowMore = () => {
+//     setVisibleCount((prevCount) => prevCount + 1);
+//   };
+
+//   const sortByScore = (sources: Sources[]): Sources[] => {
+//     return sources.sort((a, b) => {
+//       if (a.relevant !== b.relevant) {
+//         return a.relevant ? -1 : 1;
+//       }
+//       return b.score - a.score;
+//     });
+//   };
+
+//   const handleOrderChange = (newOrder: string[]) => {
+//     // console.log(newOrder);
+//     setSortedOrder(newOrder);
+//   };
+
+//   return (
+//     <div className="flex flex-col gap-2">
+//       {visibleSources.length > 0 && (
+//         <SortableList
+//           visibleSources={visibleSources}
+//           handleSourceClick={handleSourceClick}
+//           sortedOrder={sortedOrder}
+//           onOrderChange={handleOrderChange}
+//         />
+//       )}
+//       {updatedSources.length > visibleCount && (
+//         <button className="text-blue-500 my-2" onClick={handleShowMore}>
+//           Show More
+//         </button>
+//       )}
+//       <div className="mt-3 flex flex-col gap-2">
+//         {visibleSources &&
+//           visibleSources
+//             .filter((source) => !source.relevant)
+//             .map((source, i) => (
+//               <DraggableFile
+//                 key={source.title}
+//                 id={source.file_id}
+//                 index={i}
+//                 source={source}
+//                 handleSourceClick={handleSourceClick}
+//               />
+//             ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Files;
+
 'use client';
 import { Sources } from '@/interfaces/messages';
 import { useState, useEffect } from 'react';
@@ -10,41 +120,34 @@ interface FilesProps {
 }
 
 const Files = ({ sources, id }: FilesProps) => {
-  const [updatedSources, setUpdatedSources] = useState<Sources[]>([]);
   const [visibleCount, setVisibleCount] = useState(1);
   const [visibleSources, setVisibleSources] = useState<Sources[]>([]);
   const [sortedOrder, setSortedOrder] = useState<string[]>([]);
 
   useEffect(() => {
     if (sources && sources.length > 0) {
-      const relevantSources = sortByScore(
-        uniqueSources(sources.filter((source) => source.id === id))
-      );
-      setUpdatedSources(relevantSources);
+      const relevantSources = getUniqueSortedSources(sources, id);
+      setVisibleSources(relevantSources.slice(0, visibleCount));
       setSortedOrder(relevantSources.map((source) => source.file_id));
     }
-  }, [sources, id]);
+  }, [sources, id, visibleCount]);
 
-  useEffect(() => {
-    setVisibleSources((prevVisibleSources) => [
-      ...prevVisibleSources,
-      ...updatedSources.slice(prevVisibleSources.length, visibleCount),
-    ]);
-    setSortedOrder((prevVisibleSources) => [
-      ...prevVisibleSources,
-      ...updatedSources
-        .slice(prevVisibleSources.length, visibleCount)
-        .map((source) => source.file_id),
-    ]);
-  }, [updatedSources, visibleCount]);
-
-  const uniqueSources = (sources: Sources[]): Sources[] => {
-    const seen = new Set();
-    return sources.filter((source) => {
-      const duplicate = seen.has(source.title);
-      seen.add(source.title);
-      return !duplicate;
-    });
+  const getUniqueSortedSources = (
+    sources: Sources[],
+    id: string
+  ): Sources[] => {
+    const uniqueSources = Array.from(
+      new Map(
+        sources
+          .filter((source) => source.id === id)
+          .map((source) => [source.title, source])
+      ).values()
+    );
+    return uniqueSources.sort(
+      (a, b) =>
+        b.score - a.score ||
+        (a.relevant === b.relevant ? 0 : a.relevant ? -1 : 1)
+    );
   };
 
   const handleSourceClick = (source: Sources) => {
@@ -59,17 +162,14 @@ const Files = ({ sources, id }: FilesProps) => {
     setVisibleCount((prevCount) => prevCount + 1);
   };
 
-  const sortByScore = (sources: Sources[]): Sources[] => {
-    return sources.sort((a, b) => {
-      if (a.relevant !== b.relevant) {
-        return a.relevant ? -1 : 1;
-      }
-      return b.score - a.score;
-    });
-  };
-
   const handleOrderChange = (newOrder: string[]) => {
     setSortedOrder(newOrder);
+    setVisibleSources((prevVisibleSources) =>
+      newOrder.map(
+        (file_id) =>
+          prevVisibleSources.find((source) => source.file_id === file_id)!
+      )
+    );
   };
 
   return (
@@ -82,24 +182,24 @@ const Files = ({ sources, id }: FilesProps) => {
           onOrderChange={handleOrderChange}
         />
       )}
-      {updatedSources.length > visibleCount && (
+      {visibleSources.length <
+        sources.filter((source) => source.id === id).length && (
         <button className="text-blue-500 my-2" onClick={handleShowMore}>
           Show More
         </button>
       )}
       <div className="mt-3 flex flex-col gap-2">
-        {visibleSources &&
-          visibleSources
-            .filter((source) => !source.relevant)
-            .map((source, i) => (
-              <DraggableFile
-                key={source.title}
-                id={source.file_id}
-                index={i}
-                source={source}
-                handleSourceClick={handleSourceClick}
-              />
-            ))}
+        {visibleSources
+          .filter((source) => !source.relevant)
+          .map((source, i) => (
+            <DraggableFile
+              key={source.title}
+              id={source.file_id}
+              index={i}
+              source={source}
+              handleSourceClick={handleSourceClick}
+            />
+          ))}
       </div>
     </div>
   );
