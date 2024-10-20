272cf3568b1be2e64b180f52fb3473f203965628
Roman Stix <rmnstix@gmail.com>
2024-10-20 18:12:04 +0200

    chat page clean up

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,18 +3,13 @@ import { options } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth/next';
 import { redirect } from 'next/navigation';
 import Chat from '@/components/chat';
-import { getSessionUser } from '@/utils/get-session-user';
 
 const ChatPage = async () => {
   const session = await getServerSession(options);
 
   if (!session) {
     redirect('/api/auth/signin?callbackUrl=/chat');
-    // redirect('/signin?callbackUrl=/');
   }
-
-  const sessionUser = await getSessionUser();
-  // console.log(sessionUser);
 
   return (
     <AuthProvider>
@@ -22,7 +17,6 @@ const ChatPage = async () => {
         className="flex h-screen flex-col items-center justify-between"
         suppressHydrationWarning
       >
-        {/* <ServerLoading></ServerLoading> */}
         <Chat></Chat>
       </div>
     </AuthProvider>
